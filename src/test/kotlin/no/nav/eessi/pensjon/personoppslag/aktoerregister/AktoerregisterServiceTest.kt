296174aae471a90327679025372c34cedae20e9e
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2020-07-06 23:11:10 +0200

    Introduce new API for AktoerregisterService
    - Type-safe'r
    - No nulls as result
    - No throwing of exceptions

--- src/test/kotlin/no/nav/eessi/pensjon/personoppslag/aktoerregister/AktoerregisterServiceTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/personoppslag/aktoerregister/AktoerregisterServiceTest.kt
@@ -5,6 +5,7 @@ import com.nhaarman.mockitokotlin2.doThrow
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -19,7 +20,6 @@ import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import java.nio.file.Files
 import java.nio.file.Paths
-
 
 @ExtendWith(MockitoExtension::class)
 class AktoerregisterServiceTest {
@@ -54,37 +54,37 @@ class AktoerregisterServiceTest {
         val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-1-gjeldende-NorskIdent.json")
         whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
 
-        val testAktoerId = "18128126178"
-        val expectedNorskIdent = "1000101917358"
+        val inputNorskIdent = "18128126178"
+        val expectedAktoerId = "1000101917358"
 
-        val response = aktoerregisterService.hentGjeldendeAktorIdForNorskIdent(testAktoerId)
-        assertEquals(expectedNorskIdent, response,"NorskIdent 18128126178 skal ha AktoerId 100010191735818128126178")
+        val response = aktoerregisterService.hentGjeldendeAktorIdForNorskIdent(inputNorskIdent)
+        assertEquals(expectedAktoerId, response,"NorskIdent 18128126178 skal ha AktoerId 100010191735818128126178")
     }
 
 
     @Test
     fun `hentGjeldendeNorskIdentForAktorId() should fail if ident is not found in response`() {
+        // the mock returns NorskIdent 18128126178, not 1234 as we ask for
         val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-1-gjeldende-AktoerId.json")
         whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
 
         val testAktoerId = "1234"
 
         val rte = assertThrows<AktoerregisterIkkeFunnetException> {
-            // the mock returns NorskIdent 18128126178, not 1234 as we asked for
             aktoerregisterService.hentGjeldendeNorskIdentForAktorId(testAktoerId)
         }
-        assertTrue(rte.message!!.contains(testAktoerId), "Exception skal si noe om hvilken identen som ikke ble funnet")
+        assertTrue(rte.message!!.contains(testAktoerId))
     }
 
     @Test
     fun `should throw runtimeexception if no ident is found in response`() {
+        // the mock returns a valid response, but has no idents
         val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_0-IdentinfoForAktoer.json")
         whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
 
         val testAktoerId = "18128126178"
 
         val rte = assertThrows<AktoerregisterIkkeFunnetException> {
-            // the mock returns a valid response, but has no idents
             aktoerregisterService.hentGjeldendeNorskIdentForAktorId(testAktoerId)
         }
         assertTrue(rte.message!!.contains(testAktoerId), "Exception skal si noe om hvilken identen som ikke ble funnet")
@@ -93,13 +93,13 @@ class AktoerregisterServiceTest {
 
     @Test
     fun `AktoerregisterException should be thrown when response contains a 'feilmelding'`() {
+        // the mock returns a valid response, but with a message in 'feilmelding'
         val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-errormsg.json")
         whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
 
         val testAktoerId = "10000609641830456"
 
         val are = assertThrows<AktoerregisterException> {
-            // the mock returns a valid response, but with a message in 'feilmelding'
             aktoerregisterService.hentGjeldendeNorskIdentForAktorId(testAktoerId)
         }
         assertEquals("Den angitte personidenten finnes ikke", are.message!!, "Feilmeldingen fra aktørregisteret skal være exception-message")
@@ -107,31 +107,143 @@ class AktoerregisterServiceTest {
 
     @Test
     fun `should throw runtimeexception when multiple idents are returned`() {
+        // the mock returns a valid response, but has 2 gjeldende AktoerId
         val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-2-gjeldende-AktoerId.json")
         whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
 
         val testAktoerId = "1000101917358"
 
         val rte = assertThrows<AktoerregisterException> {
-            // the mock returns a valid response, but has 2 gjeldende AktoerId
             aktoerregisterService.hentGjeldendeAktorIdForNorskIdent(testAktoerId)
         }
-        assertEquals("Forventet 1 ident, fant 2", rte.message!!,"RuntimeException skal kastes dersom mer enn 1 ident returneres")
+        assertEquals("Forventet 1 gjeldende AktoerId, fant 2", rte.message!!)
     }
 
     @Test
     fun `should throw runtimeexception when 403-forbidden is returned`() {
-
+        // the mock returns 403-forbidden
         doThrow(HttpClientErrorException(HttpStatus.FORBIDDEN))
                 .whenever(mockrestTemplate).exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))
 
         val testAktoerId = "does-not-matter"
 
         val rte = assertThrows<AktoerregisterException> {
-            // the mock returns 403-forbidden
             aktoerregisterService.hentGjeldendeAktorIdForNorskIdent(testAktoerId)
         }
-        assertEquals("Received 403 FORBIDDEN Forbidden from aktørregisteret", rte.message!!, "RuntimeException skal kastes dersom mer enn 1 ident returneres")
+        assertTrue(rte.message!!.contains("403 FORBIDDEN"))
+    }
+
+    @Test
+    fun `hentGjeldendeIdentFraGruppe() should return 1 NorskIdent`() {
+        val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-1-gjeldende-AktoerId.json")
+        whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
+
+        val input = AktoerId("1000101917358")
+        val expected = NorskIdent("18128126178")
+
+        when (val result =
+                aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.NorskIdent, input)) {
+            is Result.Found -> assertEquals(expected, result.value)
+            else -> fail()
+        }
+    }
+
+
+    @Test
+    fun `hentGjeldendeIdentFraGruppe() should return 1 AktoerId`() {
+        val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-1-gjeldende-NorskIdent.json")
+        whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
+
+        val input = NorskIdent("18128126178")
+        val expected = AktoerId("1000101917358")
+
+        val result = aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.AktoerId, input)
+
+        when(result) {
+            is Result.Found -> assertEquals(expected, result.value)
+            else -> fail()
+        }
+    }
+
+
+    @Test
+    fun `hentGjeldendeIdentFraGruppe() should return NotFound if ident is not found in response`() {
+        // the mock returns NorskIdent 18128126178, not 1234 as we ask for
+        val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-1-gjeldende-AktoerId.json")
+        whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
+
+        val input = AktoerId("1234")
+
+        val result = aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.NorskIdent, input)
+
+        when(result) {
+            is Result.NotFound -> assertTrue(result.reason.contains(input.id), "Melding skal si noe om hvilke ident som ikke ble funnet")
+            else -> fail()
+        }
+    }
+
+    @Test
+    fun `hentGjeldendeIdentFraGruppe should return NotFound if no ident is found in response`() {
+        // the mock returns a valid response, but has no idents
+        val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_0-IdentinfoForAktoer.json")
+        whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
+
+        val input = AktoerId("18128126178")
+
+        val result = aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.NorskIdent, input)
+
+        when (result) {
+            is Result.NotFound -> assertTrue(result.reason.contains(input.id), "Melding skal si noe om hvilke ident som ikke ble funnet")
+            else -> fail()
+        }
+    }
+
+
+    @Test
+    fun `hentGjeldendeIdentFraGruppe should return Failure when response contains a 'feilmelding'`() {
+        // the mock returns a valid response, but with a message in 'feilmelding'
+        val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-errormsg.json")
+        whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
+
+        val input = AktoerId("10000609641830456")
+
+        val result = aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.NorskIdent, input)
+
+        when(result) {
+            is Result.Failure -> assertEquals("Den angitte personidenten finnes ikke", result.cause.message!!, "Feilmeldingen fra aktørregisteret skal være exception-message")
+            else -> fail()
+        }
+    }
+
+    @Test
+    fun `hentGjeldendeIdentFraGruppe should return Failure when multiple idents are returned`() {
+        val mockResponseEntity = createResponseEntityFromJsonFile("src/test/resources/aktoerregister/200-OK_1-IdentinfoForAktoer-with-2-gjeldende-AktoerId.json")
+        whenever(mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))).thenReturn(mockResponseEntity)
+
+        val input = NorskIdent("1000101917358")
+
+        val result = aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.AktoerId, input)
+
+        when(result) {
+            is Result.Failure -> assertEquals("Forventet 1 gjeldende AktoerId, fant 2", result.cause.message!!)
+            else -> fail()
+        }
+
+    }
+
+    @Test
+    fun `hentGjeldendeIdentFraGruppe should return Failure when 403-forbidden is returned`() {
+        doThrow(HttpClientErrorException(HttpStatus.FORBIDDEN))
+                .whenever(mockrestTemplate).exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java))
+
+        val input = NorskIdent("does-not-matter")
+
+        val result = aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.AktoerId, input)
+
+        when(result) {
+            is Result.Failure -> assertTrue(result.cause.message!!.contains("403 FORBIDDEN"))
+            else -> fail()
+        }
     }
 
     private fun createResponseEntityFromJsonFile(filePath: String, httpStatus: HttpStatus = HttpStatus.OK): ResponseEntity<String> {
