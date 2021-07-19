c5b979721371210566c751ec7b8fe8ae7157f481
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2021-07-19 15:16:27 +0200

    fiks user-system-token

--- src/main/kotlin/no/nav/eessi/pensjon/personoppslag/pdl/PdlConfiguration.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/personoppslag/pdl/PdlConfiguration.kt
@@ -7,7 +7,8 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Lazy
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpRequest
 import org.springframework.http.client.BufferingClientHttpRequestFactory
@@ -66,7 +67,7 @@ open class PdlConfiguration {
 open class PdlTokenConfiguration(private val securityTokenExchangeService: STSService) {
 
         @Bean
-        @Lazy
+        @Order(Ordered.LOWEST_PRECEDENCE)
         open fun pdlTokenCallback(): PdlTokenCallBack {
             return PdlSystemOidcToken(securityTokenExchangeService)
         }
