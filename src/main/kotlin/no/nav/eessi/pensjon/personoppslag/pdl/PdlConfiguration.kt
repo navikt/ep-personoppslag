153c2f3c13af6621437a2b5732c41b4543b3ced7
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2022-01-28 15:34:06 +0100

    fikser azuread for begrens-innsyn
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/personoppslag/pdl/PdlConfiguration.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/personoppslag/pdl/PdlConfiguration.kt
@@ -2,6 +2,7 @@ package no.nav.eessi.pensjon.personoppslag.pdl
 
 import no.nav.eessi.pensjon.logging.RequestIdHeaderInterceptor
 import no.nav.eessi.pensjon.logging.RequestResponseLoggerInterceptor
+import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.slf4j.LoggerFactory
@@ -19,23 +20,26 @@ import org.springframework.web.client.DefaultResponseErrorHandler
 import org.springframework.web.client.RestTemplate
 import java.util.*
 
+@Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @Configuration
 class PdlConfiguration {
 
     @Bean
     fun pdlRestTemplate(templateBuilder: RestTemplateBuilder, clientConfigurationProperties: ClientConfigurationProperties, oAuth2AccessTokenService: OAuth2AccessTokenService): RestTemplate {
+        val clientProperties =  Optional.ofNullable(clientConfigurationProperties.registration["pdl-credentials"]).orElseThrow { RuntimeException("could not find oauth2 client config for pdl-credentials") }
+
         return templateBuilder
             .errorHandler(DefaultResponseErrorHandler())
             .additionalInterceptors(
                 RequestIdHeaderInterceptor(),
                 RequestResponseLoggerInterceptor(),
-                PdlInterceptor(clientConfigurationProperties, oAuth2AccessTokenService))
+                PdlInterceptor(clientProperties, oAuth2AccessTokenService))
             .build().apply {
                 requestFactory = BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory())
             }
     }
 
-    internal class PdlInterceptor(private val clientConfigurationProperties: ClientConfigurationProperties, private val oAuth2AccessTokenService: OAuth2AccessTokenService) : ClientHttpRequestInterceptor {
+    internal class PdlInterceptor(private val clientProperties: ClientProperties, private val oAuth2AccessTokenService: OAuth2AccessTokenService) : ClientHttpRequestInterceptor {
 
         private val logger = LoggerFactory.getLogger(PdlInterceptor::class.java)
 
@@ -43,7 +47,6 @@ class PdlConfiguration {
                                body: ByteArray,
                                execution: ClientHttpRequestExecution): ClientHttpResponse {
 
-            val clientProperties =  Optional.ofNullable(clientConfigurationProperties.registration["pdl-credentials"]).orElseThrow { RuntimeException("could not find oauth2 client config for pdl-credentials") }
             val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
             val token = response.accessToken
 
