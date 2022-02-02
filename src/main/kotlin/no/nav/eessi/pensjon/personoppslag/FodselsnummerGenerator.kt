7a46e180c9b99a1f6f4fd00654bb94eb296032b9
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2022-02-02 10:57:16 +0100

    Forbedret Fnrgenerator
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/personoppslag/FodselsnummerGenerator.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/personoppslag/FodselsnummerGenerator.kt
@@ -1,44 +1,99 @@
 package no.nav.eessi.pensjon.personoppslag
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
 
 object FodselsnummerGenerator {
 
+    private val vekttallProviderFnr1: (Int) -> Int = { arrayOf(3, 7, 6, 1, 8, 9, 4, 5, 2).reversedArray()[it] }
+    private val vekttallProviderFnr2: (Int) -> Int = { arrayOf(5, 4, 3, 2, 7, 6, 5, 4, 3, 2).reversedArray()[it] }
+    private val fnrDateFormat = DateTimeFormatter.ofPattern("ddMMyy")
+    private val KUN_SIFFER = Regex("\\d+")
+
     /**
-    Kun for generering av fnr for test
+     * Ide tatt fra Coronapenger søknad-api https://github.com/navikt/coronapenger-soknad-api
+       Kun for generering av fnr for test
      */
     fun generateFnrForTest(alder: Int): String {
-        var dag = 10
-        var kontrollsiffer = 10
-        val fnrdate = LocalDate.now().minusYears(alder.toLong() +1)
-        val year = fnrdate.year.toString()
-        val month = withLeadingZero(fnrdate.month.minus (1).value.toString())
-        val fixedyear = year.substring(2, year.length)
-        var fnr: String
-        var fodselsnummer: Fodselsnummer? = null
-        var errorCounter = 0
-        do {
-            fnr = "$dag" + month + fixedyear + indvididnr(fnrdate.year) + kontrollsiffer
-            fodselsnummer = try {
-                Fodselsnummer.fraMedValidation(fnr)
-            } catch (ex: Exception) {
-                if (ex.message == "Ugyldig kontrollnummer") {
-                    kontrollsiffer++
-                }
-                errorCounter++
-                null
-            }
-            if (kontrollsiffer >= 99) {
-                kontrollsiffer = 10
-                dag++
-            }
-            if (dag > 30) dag = 10
-            if (errorCounter > 250) break;
-            //println("fodselsnummer: $fodselsnummer, alder: ${fodselsnummer?.getAge()}")
-        } while (fodselsnummer == null || (fodselsnummer.getAge() != alder))
-        return fodselsnummer.toString()
+        var fnr: String? = null
+        while (fnr == null) {
+            fnr = genFnr(alder)
+            if (fnr.contains("-") || !fnr.erGyldigFodselsnummer()) fnr = null
+        }
+        return fnr
     }
-    private fun withLeadingZero(str: String) = if (str.length == 1) "0$str" else str
+
+    private fun String.erKunSiffer() = matches(KUN_SIFFER)
+    private fun String.starterMedFodselsdato(): Boolean {
+        return try {
+            fnrDateFormat.parse(substring(0, 6))
+            true
+        } catch (cause: Throwable) {
+            false
+        }
+    }
+
+    private fun String.erGyldigFodselsnummer(): Boolean {
+        if (length != 11 || !erKunSiffer() || !starterMedFodselsdato()) return false
+        val forventetKontrollsifferEn = get(9)
+        val kalkulertKontrollsifferEn = Mod11.kontrollsiffer(
+            number = substring(0, 9),
+            vekttallProvider = vekttallProviderFnr1
+        )
+        if (kalkulertKontrollsifferEn != forventetKontrollsifferEn) return false
+        val forventetKontrollsifferTo = get(10)
+        val kalkulertKontrollsifferTo = Mod11.kontrollsiffer(
+            number = substring(0, 10),
+            vekttallProvider = vekttallProviderFnr2
+        )
+        return kalkulertKontrollsifferTo == forventetKontrollsifferTo
+    }
+
+    private fun genFnr(alder: Int): String {
+        val fnrdate = LocalDate.now().minusYears(alder.toLong())
+        var year = fnrdate.year
+        val dagraw = Random.nextInt(1, 29)
+        val maaneraw = Random.nextInt(1, 13)
+        //sjekk for om random gir en gyldig dato lik alder.
+        val check = LocalDate.of(year, maaneraw, dagraw)
+        if (check.isAfter(fnrdate)) {
+            year -= 1
+        }
+        val dag = dagraw.toString().padStart(2, '0')
+        val maaned = maaneraw.toString().padStart(2, '0')
+        val aar = year.toString().substring(2, year.toString().length)
+        val individSiffer = indvididnr(fnrdate.year)
+        val utenKontrollSiffer = "$dag$maaned$aar$individSiffer"
+        val medForsteKontrollsiffer = utenKontrollSiffer + Mod11.kontrollsiffer(
+            number = utenKontrollSiffer,
+            vekttallProvider = vekttallProviderFnr1
+        )
+        //println("fnrdate: $fnrdate, check: $check, year: $year, dag: $dag, måned: $maaned, år: $aar, FNR: $medForsteKontrollsiffer")
+        return medForsteKontrollsiffer + Mod11.kontrollsiffer(
+            number = medForsteKontrollsiffer,
+            vekttallProvider = vekttallProviderFnr2
+        )
+    }
+
+    internal object Mod11 {
+        private val defaultVekttallProvider: (Int) -> Int = { 2 + it % 6 }
+        internal fun kontrollsiffer(
+            number: String,
+            vekttallProvider: (Int) -> Int = defaultVekttallProvider
+        ): Char {
+            return number.reversed().mapIndexed { i, char ->
+                Character.getNumericValue(char) * vekttallProvider(i)
+            }.sum().let(Mod11::kontrollsifferFraSum)
+        }
+        private fun kontrollsifferFraSum(sum: Int) = sum.rem(11).let { rest ->
+            when (rest) {
+                0 -> '0'
+                1 -> '-'
+                else -> "${11 - rest}"[0]
+            }
+        }
+    }
     private fun indvididnr(year: Int): String {
             val id = when (year) {
                 in 1900..1999 -> "433"
@@ -49,3 +104,4 @@ object FodselsnummerGenerator {
         return id
     }
 }
+
